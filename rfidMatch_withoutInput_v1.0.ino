5a0babd78bfbdc06510e5aa6cea460890aaaf50e
Aniruddha Arondekar <36357874+Crowned-Eagle@users.noreply.github.com>
2018-02-11 13:24:46 +0530

    Create rfidMatch_withoutInput_v1.0.ino

---
+++ rfidMatch_withoutInput_v1.0.ino
@@ -0,0 +1,30 @@
+int count = 0;                                          // count = 0
+char input[12];                                         // character array of size 12
+char code2match[12] = {'1', '1', '0', '0', '7', '7', '9', 'B', 'D', '7', '2', 'A'};
+boolean flag = 0;                                       // flag =0
+void setup()
+{
+  Serial.begin(9600);                                  // begin serial port with baud rate 9600bps
+
+}
+void loop()
+{
+  if (Serial.available())
+  {
+    Serial.readBytes(input, 12);
+    delayMicroseconds(50);
+
+    Serial.print("Card ID - ");
+    Serial.println(input);      // Print RFID tag number
+
+    for (count = 0; count < 12; count++)
+    {
+      if (input[count] != code2match[count])
+        break;
+    }
+    if (count == 12)
+      Serial.println("Yes, we got it!");
+    //digitalWrite(13, HIGH);
+
+  }
+}
