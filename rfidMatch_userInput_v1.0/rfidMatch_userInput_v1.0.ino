c0014f15a7ed51a1a1942d61131ff3914f1f4042
Crowned Eagle <aniruddha.arondekar@gmail.com>
2018-02-14 00:09:38 +0530

    Modified the file 'rfidMatch_userInput_v1.0' - Now It works for all no. i/p characters >5 by taking 1st 5 characters and rejeting rest of all AND if no. of i/p characters are <5 then it reject that i/p altogether

--- rfidMatch_userInput_v1.0/rfidMatch_userInput_v1.0.ino
+++ rfidMatch_userInput_v1.0/rfidMatch_userInput_v1.0.ino
@@ -1,29 +1,33 @@
-int count = 0;                                          // count = 0
+int count = 0;                                    // count = 0
 char userInput[5];
-//char code2match[12] = {'1', '1', '0', '0', '7', '7', '9', 'B', 'D', '7', '2', 'A'};
+
 void setup()
 {
+  //delay(1000);
   Serial.begin(9600);                            // begin serial port with baud rate 9600bps
 
 }
 void loop()
 {
-    if (Serial.available() > 5)
+  if (Serial.available() < 5)
+  {
+    Serial.read();
+  }
+  else if (Serial.available() >= 5)
+  {
+    for (int i = 0; i < 5; i++)
     {
-       Serial.read();
+      userInput[i] = Serial.read();
     }
     
-    else if (Serial.available() < 5)
+    while (Serial.available() > 0)
     {
-    	Serial.read();
+      Serial.read();
     }
     
-    else
-    {
-       Serial.readBytes(userInput, 5);
-       Serial.print("User Input - ");
-       Serial.println(userInput);
-    }
+    Serial.print("User Input - ");
+    Serial.println(userInput);
+  }
 
-  delay(1000);
+  delay(100);
 }
