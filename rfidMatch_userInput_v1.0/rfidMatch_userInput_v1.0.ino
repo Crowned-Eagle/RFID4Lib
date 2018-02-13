b1a7a8aa4b3691184d146debeca77cab8ca482cd
Crowned-Eagle <aniruddha.arondekar@gmail.com>
2018-02-13 10:41:48 +0530

    Test my chsnges in User input (beta)

--- rfidMatch_userInput_v1.0/rfidMatch_userInput_v1.0.ino
+++ rfidMatch_userInput_v1.0/rfidMatch_userInput_v1.0.ino
@@ -3,20 +3,25 @@ char userInput[5];
 //char code2match[12] = {'1', '1', '0', '0', '7', '7', '9', 'B', 'D', '7', '2', 'A'};
 void setup()
 {
-  Serial.begin(9600);                                  // begin serial port with baud rate 9600bps
+  Serial.begin(9600);                            // begin serial port with baud rate 9600bps
 
 }
 void loop()
 {
-  while (Serial.available() > 5)
-    Serial.read();
-  
-    //Serial.println("We got 5 bytes");
-    Serial.readBytes(userInput, 5);
-    Serial.print("User Input - ");
-    Serial.println(userInput);
-  
+    if (Serial.available() > 5)
+    { Serial.read(); }
+    
+    else if (Serial.available() < 5)
+    {
+    	
+    }
+    
+    else
+    {
+       Serial.readBytes(userInput, 5);
+       Serial.print("User Input - ");
+       Serial.println(userInput);
+    }
 
   delay(1000);
-
 }
